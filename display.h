55d234c6d8287aebbf2d75a41be315308a79c0e9
Ketaki Kulkarni <140495569+Kool-K@users.noreply.github.com>
2024-11-23 00:12:11 +0530

    Create display.h

---
+++ display.h
@@ -0,0 +1,11 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "HospitalManagement.h"
+
+void display_all_patients(const HospitalManagement& hospital);
+void display_all_doctors(const HospitalManagement& hospital);
+void display_all_appointments(const HospitalManagement& hospital);
+void display_all_bills(const HospitalManagement& hospital);
+
+#endif
